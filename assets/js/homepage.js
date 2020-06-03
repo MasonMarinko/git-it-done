20268726d85b56faefc85694609ac638e3ac359c
MasonM <Mason.P.Marinko@gmail.com>
2020-06-03 12:32:47 -0600

    Connected index to single-repo

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -59,8 +59,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     //=========create a container for each repo on HTML=======//
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center"
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     //========= Create a Span element to hold the repository name=======//
         var titleEl = document.createElement("span");
