edf1af8df64bf2c4bdfee357c15677dd3106b0e2
MasonM <Mason.P.Marinko@gmail.com>
2020-06-03 15:43:02 -0600

    fix#3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -11,24 +11,24 @@ var getUserRepos = function (user) {
 
     //========make a request to the url===================//
     fetch(apiUrl)
-    .then(function(response) {
-        //==========request is successful==============//
-        if (response.ok) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
+        .then(function (response) {
+            //==========request is successful==============//
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            //=====Notice this '.catch()' getting chained onto the end of the '.then()'
+            alert("Unable to connect to GitHub");
         });
-    } else {
-        alert("Error: " + response.statusText);
-    }
-    })
-    .catch(function(error) {
-        //=====Notice this '.catch()' getting chained onto the end of the '.then()'
-        alert("Unable to connect to GitHub");
-    });
 };
 
 //===========Using this to capture the information put inside the input field====//
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     //====get value from input element=====//
     var username = nameInputEl.value.trim();
@@ -42,12 +42,12 @@ var formSubmitHandler = function(event) {
 }
 
 //=========== Allows for search with certain perameters======//
-var getFeaturedRepos = function(language) {
+var getFeaturedRepos = function (language) {
     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featurted&sort=help-wanted-issues";
 
-    fetch(apiUrl).then(function(response) {
+    fetch(apiUrl).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 displayRepos(data.items, language);
             })
         } else {
@@ -58,7 +58,7 @@ var getFeaturedRepos = function(language) {
 
 
 //========using this to properly show the information we actually want=====//
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
     //=============== check if api (search) returned any repos or user has none====//
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
@@ -71,41 +71,41 @@ var displayRepos = function(repos, searchTerm) {
 
 
     // loop over repos
-    for (var i=0; i < repos.length; i++) {
-    //=======Format repo name=======//
+    for (var i = 0; i < repos.length; i++) {
+        //=======Format repo name=======//
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    //=========create a container for each repo on HTML=======//
+        //=========create a container for each repo on HTML=======//
         var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center"
         repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
-    //========= Create a Span element to hold the repository name=======//
+        //========= Create a Span element to hold the repository name=======//
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
 
-    //======== append it to the container ===================//
+        //======== append it to the container ===================//
         repoEl.appendChild(titleEl);
 
-    // ======= creating a status element=================//
+        // ======= creating a status element=================//
         var statusEl = document.createElement("span");
         statusEl.classList = "flex-row align-center";
 
-    //========= IF statement to show issues in the dom on each item=============//
+        //========= IF statement to show issues in the dom on each item=============//
         if (repos[i].open_issues_count > 0) {
-            statusEl.innerHTML = 
-                "<i class='fas fa-times status-icon icon-danger'></i>"+ repos[i].open_issues_count + " issue(s)";            
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
 
         repoEl.appendChild(statusEl);
-    //================ append container to the dom ==========//
+        //================ append container to the dom ==========//
         repoContainerEl.appendChild(repoEl);
     }
 };
 
-var buttonClickHandler = function(event) {
+var buttonClickHandler = function (event) {
     var language = event.target.getAttribute("data-language")
     if (language) {
         getFeaturedRepos(language);
